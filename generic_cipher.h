c2a9ed19361fe30b3c270025613f1d59c13a6419
amr2356 <81318928+amr2356@users.noreply.github.com>
2021-04-06 16:31:42 -0400

    Update generic_cipher.h

--- generic_cipher.h
+++ generic_cipher.h
@@ -10,7 +10,6 @@ public:
 	GenericCipherText(string t): encrypted{t}, decrypted{t}, length{t.length()}, score{min_quadgram_score} {}
 	GenericCipherText(): encrypted{}, decrypted{}, length{0}, score{min_quadgram_score} {}
 	void read_decrypted() {cin >> decrypted; encrypted=decrypted; length=decrypted.length();}
-	void read_encrypted() {cin >> encrypted; decrypted=encrypted; length=encrypted.length();}
 	bool cryptanalysis() {return true;}
 	string settings() {return "Nothing!";}
 };
