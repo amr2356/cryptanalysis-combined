b5b9cf5c85cb1eacbf62848789c056f33243a4c4
amr2356 <81318928+amr2356@users.noreply.github.com>
2021-04-06 18:19:46 -0400

    Add files via upload

--- cryptanalysis.cpp
+++ cryptanalysis.cpp
@@ -10,20 +10,20 @@
 
 class Cipher {
 public:
-	std::string label, decrypted, settings;
+	std::string decrypted, settings;
 	double time_taken;
-	Cipher(std::string l, std::string d, std::string s, double t): label{l}, decrypted{d}, settings{s}, time_taken{t} {}
+	Cipher(std::string d, std::string s, double t): decrypted{d}, settings{s}, time_taken{t} {}
 };
 
 template<typename T>
-Cipher cryptanalysis(std::string label, T ciphertext) {
+Cipher timed_analysis(std::string label, T ciphertext) {
 	using namespace std::chrono;
 	auto start = high_resolution_clock::now();
 	bool success = ciphertext.cryptanalysis();
 	auto end = high_resolution_clock::now();
 	double time_ms = duration_cast<milliseconds>(end-start).count();
-	Cipher result {label, ciphertext.decrypted, ciphertext.settings(), time_ms/1000.0};
-	if(success) std::cout << '\n' << result.label << ":\n" << result.decrypted << '\n' << result.settings << "\nTime taken: " << result.time_taken << " s\n";
+	Cipher result {ciphertext.decrypted, ciphertext.settings(), time_ms/1000.0};
+	if(success) std::cout << '\n' << label << ":\n" << result.decrypted << '\n' << result.settings << "\nTime taken: " << result.time_taken << " s\n";
 	return result;
 }
 
@@ -36,10 +36,10 @@ int main(){
 	
 	//Add or remove ciphers here
 	std::vector<std::future<Cipher>> cipher_tasks;
-	cipher_tasks.push_back(std::async([&]{return cryptanalysis("Vigenere", VigenereText(input_text));}));
-	cipher_tasks.push_back(std::async([&]{return cryptanalysis("Commercial Enigma", EnigmaText(input_text));}));
-	cipher_tasks.push_back(std::async([&]{return cryptanalysis("Military Enigma", EnigmaPlugboardText(input_text));}));
-	//cipher_tasks.push_back(std::async([&]{return cryptanalysis("Nothing", GenericCipherText(input_text));}));
+	cipher_tasks.push_back(std::async([&]{return timed_analysis("Vigenere", VigenereText(input_text));}));
+	cipher_tasks.push_back(std::async([&]{return timed_analysis("Commercial Enigma", EnigmaText(input_text));}));
+	cipher_tasks.push_back(std::async([&]{return timed_analysis("Military Enigma", EnigmaPlugboardText(input_text));}));
+	//cipher_tasks.push_back(std::async([&]{return timed_analysis("Nothing", GenericCipherText(input_text));}));
 	
 	return 0;
 }
