486bbe26dacea696f123b531fc77e19264a9e47c
amr2356 <81318928+amr2356@users.noreply.github.com>
2021-04-07 04:05:15 -0400

    Add files via upload

--- generic_cipher.h
+++ generic_cipher.h
@@ -10,6 +10,6 @@ public:
 	GenericCipherText(string t): encrypted{t}, decrypted{t}, length{t.length()}, score{min_quadgram_score} {}
 	GenericCipherText(): encrypted{}, decrypted{}, length{0}, score{min_quadgram_score} {}
 	void read_decrypted() {cin >> decrypted; encrypted=decrypted; length=decrypted.length();}
-	bool cryptanalysis() {return true;}
-	string settings() {return "Nothing!";}
+	virtual bool cryptanalysis() = 0;
+	virtual string settings() = 0;
 };
